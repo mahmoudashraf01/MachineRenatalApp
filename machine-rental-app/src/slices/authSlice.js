0d4abe1ba7107ab63bca3260613e9468558e226b
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-11-20 15:19:12 +0200

    adding auth reducers

---
+++ machine-rental-app/src/slices/authSlice.js
@@ -0,0 +1,84 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// ==========================
+// Register Thunk
+// ==========================
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (formData, { rejectWithValue }) => {
+    try {
+      // Create full_name
+      const full_name = `${formData.first_name} ${formData.last_name}`;
+
+      // Extract needed fields only
+      const body = {
+        first_name: full_name,
+        last_name: full_name,
+        company_name: formData.company_name,
+        email: formData.email,
+        password: formData.password,
+        password_confirmation: formData.password_confirmation,
+      };
+
+      const response = await axios.post(
+        "https://darkgray-bee-896770.hostingersite.com/api/register",
+        body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return response.data;
+
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Error");
+    }
+  }
+);
+
+// ==========================
+// Slice
+// ==========================
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: false,
+    user: null,
+    token: null,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Pending
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+
+      // Fulfilled
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+
+        // user Data
+        state.user = action.payload.data.user;
+
+        // token
+        state.token = action.payload.data.token;
+
+        // save token
+        localStorage.setItem("token", action.payload.data.token);
+      })
+
+      // Rejected
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default authSlice.reducer;
