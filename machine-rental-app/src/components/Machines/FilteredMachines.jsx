cebd757c0745983f0f7f32123d73598ddbb086fa
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-11-24 16:35:45 +0200

    fininshing filtered machines section

--- machine-rental-app/src/components/Machines/FilteredMachines.jsx
+++ machine-rental-app/src/components/Machines/FilteredMachines.jsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
-import MachineCard from '../../components/Landing/MachineCard';
 import { FaArrowRight } from 'react-icons/fa';
 import MachinesFilter from './MachinesFilter';
+import FilteredMachineCards from './FilteredMachineCards';
 
 
 const FilteredMachines = () => {
@@ -12,15 +12,20 @@ const FilteredMachines = () => {
         <div className=' justify-center items-center w-full bg-equipmentBg pb-10'>
             <div className='flex max-sm:flex-col max-md:flex-col justify-start max-sm:justify-center items-start max-sm:items-center max-md:items-center gap-10 px-10 py-10'>
                 <MachinesFilter />
-                <div className=' grid md:grid-cols-1 mx-auto xl:grid-cols-3 lg:grid-cols-2  sm:grid-cols-1 gap-10'>
-                    <MachineCard />
-                    <MachineCard />
-                    <MachineCard />
-                    <MachineCard />
-                    <MachineCard />
-                    <MachineCard />
+                <div className='flex flex-col gap-7 w-full'>
+                    <div className='flex lg:justify-start justify-center gap-2'>
+                        <h1 className='lg:text-[32px] text-[20px] font-bold '>Available Machines</h1>
+                        <p>(12 results)</p>
+                    </div>
+                    <div className=' grid md:grid-cols-1 mx-auto xl:grid-cols-3 lg:grid-cols-2  sm:grid-cols-1 gap-10'>
+                        <FilteredMachineCards />
+                        <FilteredMachineCards />
+                        <FilteredMachineCards />
+                        <FilteredMachineCards />
+                        <FilteredMachineCards />
+                        <FilteredMachineCards />
+                    </div>
                 </div>
-
             </div>
         </div>
     );
