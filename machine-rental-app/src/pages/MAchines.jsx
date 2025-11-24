689f50870145429364e1d31f47e693040e992550
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-11-24 16:52:41 +0200

    finishing machines page

--- machine-rental-app/src/pages/MAchines.jsx
+++ machine-rental-app/src/pages/MAchines.jsx
@@ -1,12 +1,16 @@
 import { memo } from 'react';
 import MachineHero from '../components/Machines/MachineHero';
 import FilteredMachines from '../components/Machines/FilteredMachines';
+import Serveys from '../components/Serveys';
+import Footer from '../components/Footer';
 
 const Machines = () => {
     return (
         <>
             <MachineHero />
             <FilteredMachines />
+            <Serveys />
+            <Footer />
         </>
     );
 };
